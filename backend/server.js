712c8a9e3f36a9a119dd8234ab532531ac6c615a
wdcoders <59465038+wdcoders@users.noreply.github.com>
2020-11-08 01:02:31 +0530

    db connected

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,22 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
-// Database Connection
+//! DATABASE CONNECTION
+const DBURL = 'mongodb://localhost/crud';
+mongoose.connect(DBURL, {useNewUrlParser:true, useCreateIndex:true, useUnifiedTopology:true, useFindAndModify:true});
+const connection = mongoose.connection;
+connection.once('open', ()=>{
+    console.log('Database connected...');
+}).catch(error=>{
+    console.log('Connection failed. '+error);
+});
 
-app.get('/', (req, res)=>{
-    res.send('working')
-})
-
+//! ROUTES
 require('./routes/api')(app);
 
-app.listen(3000, ()=>{
+
+//! SERVER
+const server = app.listen(3000, ()=>{
     console.log('Server connected');
 })
